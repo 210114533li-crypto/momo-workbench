c57d97e4ac2457a9ae055d4a9b1d18605db7478b
210114533li-crypto <210114533li-crypto@users.noreply.github.com>
2026-07-26 21:55:35 +0800

    Momo 每日工作台 v1.0

---
+++ sw.js
@@ -0,0 +1,3 @@
+var CACHE='momo-v1',FILES=['/','/index.html','/manifest.json','/icon.png'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES)}))});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
