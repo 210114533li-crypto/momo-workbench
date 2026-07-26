7038a2abf64f9dd4e4101644f13fe9a9dda9ce65
210114533li-crypto <210114533li-crypto@users.noreply.github.com>
2026-07-26 22:08:34 +0800

    修复 PWA 图标：多尺寸独立文件 + 优化加载速度

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-var CACHE='momo-v1',FILES=['/','/index.html','/manifest.json','/icon.png'];
+var CACHE='momo-v2',FILES=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png','/icon-1024.png','/apple-touch-icon.png'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES)}))});
 self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
